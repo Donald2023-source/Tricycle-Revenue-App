fad47858cbc674c8d2375823670a45ae46337385
Donald2023-source <donalddyusuf@gmail.com>
2026-03-11 10:30:42 +0100

    style: dashboard alignments

--- src/Components/Aside.jsx
+++ src/Components/Aside.jsx
@@ -1,72 +1,75 @@
-import React from 'react'
+import React from "react";
 import { LuLayoutDashboard } from "react-icons/lu";
 import { MdOutlinePayments } from "react-icons/md";
 import { VscNotebook } from "react-icons/vsc";
 import { CiSettings } from "react-icons/ci";
 import { IoIosLogOut } from "react-icons/io";
-import { NavLink }  from 'react-router-dom'
-import logo from '../assets/logo.jpg';
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.jpg";
 import { BiMenuAltLeft } from "react-icons/bi";
 
 const Aside = () => {
+  const navLinks = [
+    {
+      name: "Dashboard",
+      icon: <LuLayoutDashboard />,
+      to: "/dashboard",
+    },
 
-    const navLinks = [ 
-        {
-            name: 'Dashboard',
-            icon: <LuLayoutDashboard />,
-            to: '/dashboard'
-        },
+    {
+      name: "Payments",
+      icon: <MdOutlinePayments />,
+      to: "/payments",
+    },
 
-        {
-            name: "Payments", 
-            icon: <MdOutlinePayments/>,
-            to: "/payments"
-        }, 
+    {
+      name: "Transactions",
+      icon: <VscNotebook />,
+      to: "/transaction",
+    },
 
-        {
-            name: "Transactions", 
-            icon: <VscNotebook />,
-            to: '/transaction'
-        },
+    {
+      name: "Settings",
+      icon: <CiSettings />,
+      to: "/settings",
+    },
+  ];
 
-        {
-            name: "Settings",
-            icon: <CiSettings/>,
-            to: '/settings'
-        }
-    ]
-    
   return (
-      <aside className='lg:w-[22%] w-[70%] top-0 z-[100] bg-white fixed shadow-md lg:text-md text-xs  p-5 h-[100vh]'>
-          <img className='h-10 mb-5' src={logo} alt="" />
-        <div className='flex flex-col justify-between h-[80%] lg:h-[90%]'>
-
-            <div className='flex flex-col mt-5 gap-4'>
-                {navLinks.map((link, index) => {
-                    return (
-                        <div className='' key={index}>
-                            <NavLink className={(({isActive}) => `p-3 flex items-center gap-3 transition-all hover:bg-myGreen hover:text-white rounded-xl ${isActive && 'bg-myGreen text-white w-[100%] rounded-xl'}`)} to={link.to}>
-                                <span className='text-xl'>{link.icon}</span>
-                                <h2 className='text-base'>{link.name}</h2>
-                            </NavLink>
-                        </div>
-                    )
-                })}
-            </div>
-
-            <hr />
-            
-            <div className='p-3 flex flex-col gap-4'>
-                <h2>Refer a Friend</h2>
-                <h2>Support</h2>
-                <span className='flex items-center gap-3 '>
-                    Log out
-                    <IoIosLogOut />
-                </span>
-            </div>
+    <aside className="lg:w-[22%] w-[70%] top-0 z-[100] bg-white fixed shadow-md lg:text-base text-xs  p-5 h-[100vh]">
+      <img className="h-10 mb-5" src={logo} alt="" />
+      <div className="flex flex-col justify-between h-[80%] lg:h-[90%]">
+        <div className="flex flex-col mt-5 gap-4">
+          {navLinks.map((link, index) => {
+            return (
+              <div className="" key={index}>
+                <NavLink
+                  className={({ isActive }) =>
+                    `p-3 flex items-center gap-3 transition-all hover:bg-myGreen hover:text-white rounded-xl ${isActive && "bg-myGreen text-white w-[100%] rounded-xl"}`
+                  }
+                  to={link.to}
+                >
+                  <span className="text-xl">{link.icon}</span>
+                  <h2 className="md:text-base">{link.name}</h2>
+                </NavLink>
+              </div>
+            );
+          })}
         </div>
-      </aside>
-  )
-}
 
-export default Aside
+        <hr />
+
+        <div className="p-3 flex flex-col gap-4">
+          <h2>Refer a Friend</h2>
+          <h2>Support</h2>
+          <span className="flex items-center gap-3 ">
+            Log out
+            <IoIosLogOut />
+          </span>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Aside;
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,38 +1,39 @@
-import moment from 'moment/moment'
-import React, { useState } from 'react'
+import moment from "moment/moment";
+import React, { useState } from "react";
 import { MdOutlineDateRange } from "react-icons/md";
 import { LuClock3 } from "react-icons/lu";
 import { FiMoon } from "react-icons/fi";
 import { IoIosNotificationsOutline } from "react-icons/io";
-import profile from '../assets/Frame 1099.png'
-
+import profile from "../assets/Frame 1099.png";
 
 const Profile = () => {
-    const Date = moment().format('LL')
-    console.log(Date)
+  const Date = moment().format("LL");
+  console.log(Date);
 
-    const time = moment().format('LT')
+  const time = moment().format("LT");
   return (
-    <div className='md:flex hidden w-[70%] bg-white my-2 mx-auto justify-between px-6 py-2 rounded-full'>
-      <div className='flex items-center gap-4'>
-         <span><MdOutlineDateRange size={20} /></span>
-         <h2 className='text-silver'>{Date}</h2>
+    <div className="md:flex hidden w-[70%] bg-white my-2 mx-auto justify-between px-6 py-2 rounded-full">
+      <div className="flex items-center font-semibold gap-4">
+        <span>
+          <MdOutlineDateRange size={20} />
+        </span>
+        <h2 className="text-black/50">{Date}</h2>
       </div>
 
-      <div className='flex'>
-        <span className='flex items-center gap-4'>
-             <LuClock3 />
-             <h2 className='text-silver'>{time}</h2>
+      <div className="flex">
+        <span className="flex items-center font-semibold gap-4">
+          <LuClock3 />
+          <h2 className="text-black/50">{time}</h2>
         </span>
       </div>
 
-      <span className='flex items-center gap-6'>
-        <FiMoon  size={20} />
-        <IoIosNotificationsOutline  size={25} />
-        <img  className='h-9 hover:scale-105 transition-all' src={profile} />
+      <span className="flex items-center gap-6">
+        <FiMoon size={20} />
+        <IoIosNotificationsOutline size={25} />
+        <img className="h-9 hover:scale-105 transition-all" src={profile} />
       </span>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
